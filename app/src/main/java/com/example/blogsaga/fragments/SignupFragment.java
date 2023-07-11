2a92d7c1606ea9a2d3d534cff57e483d593fa7a5
Ramen Bar <ramenbar87@gmail.com>
2023-07-11 17:32:53 +0530

    sign up updated

--- app/src/main/java/com/example/blogsaga/fragments/SignupFragment.java
+++ app/src/main/java/com/example/blogsaga/fragments/SignupFragment.java
@@ -10,11 +10,6 @@ import android.view.ViewGroup;
 
 import com.example.blogsaga.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignupFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SignupFragment extends Fragment {
 
 

4b619ec09b34ec99d8231e6e05a5d62b64f5df3d
Suraj18-das <dsuraj2004@gmail.com>
2023-08-09 12:25:50 +0530

    Draftsfragment,PublishedFragments,drafts and published xml and bookmark_drafts_rv xml added Drafts attached in profile page,UpdateDraftAdapter and RecyclerDraftCallbacks added

--- app/src/main/java/com/example/blogsaga/fragments/DraftsFragment.java
+++ app/src/main/java/com/example/blogsaga/fragments/DraftsFragment.java
@@ -2,65 +2,78 @@ package com.example.blogsaga.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.blogsaga.R;
+import com.example.blogsaga.utils.adapters.UpdateDraftAdapter;
+import com.example.blogsaga.utils.callbacks.RecyclerCallbacks;
+import com.example.blogsaga.utils.callbacks.RecyclerDraftCallbacks;
+import com.example.blogsaga.utils.models.Articles;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DraftsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class DraftsFragment extends Fragment {
+import java.util.ArrayList;
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class DraftsFragment extends Fragment implements RecyclerCallbacks {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView recyclerView;
+    LinearLayoutManager LayoutManager;
+    List<Articles> dataset;
+    UpdateDraftAdapter Adapter;
 
-    public DraftsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DraftsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DraftsFragment newInstance(String param1, String param2) {
-        DraftsFragment fragment = new DraftsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_drafts, container, false);
+        View root= inflater.inflate(R.layout.fragment_drafts, container, false);
+        return root;
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView=view.findViewById(R.id.recyclerview);
+        initData();
+        initRecyclerview();
+    }
+
+
+
+    private void initRecyclerview() {
+        LayoutManager=new LinearLayoutManager(requireContext());
+        LayoutManager.setOrientation(recyclerView.VERTICAL);
+        recyclerView.setLayoutManager(LayoutManager);
+        Adapter=new UpdateDraftAdapter(dataset,getContext(), this::onClick);
+        recyclerView.setAdapter(Adapter);
+        Adapter.notifyDataSetChanged();
+
+    }
+
+
+    private void initData() {
+        dataset=new ArrayList<>();
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+        dataset.add(new Articles(R.drawable.recentpic,R.drawable.recentpic,"Nasty fitness","8 days ago","5 tips for stay fit and healthy"));
+
+    }
+    @Override
+    public void onClick(Articles articles) {
+
     }
 }
---
+++ app/src/main/java/com/example/blogsaga/utils/callbacks/RecyclerDraftCallbacks.java
@@ -0,0 +1,7 @@
+package com.example.blogsaga.utils.callbacks;
+
+import com.example.blogsaga.utils.models.Articles;
+
+public interface RecyclerDraftCallbacks {
+    void onClick(Articles articles);
+}
